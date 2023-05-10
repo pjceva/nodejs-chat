10ca4ef06c85fe09dbb9ce6a3d54a159cc4ebb94
pze <pze@pop-os.localdomain>
2023-05-09 22:14:05 -0300

    ChatsPage done

--- frontend/src/ChatsPage.jsx
+++ frontend/src/ChatsPage.jsx
@@ -1,5 +1,17 @@
-const ChatsPage = () =>{
-    return <>chats...</>
+import {MultiChatSocket, MultiChatWindow, useMultiChatLogic} from 'react-chat-engine-advanced';
+
+const ChatsPage = (props) =>{
+    const chatProps = useMultiChatLogic(
+        'ef5f7155-544e-4f0e-9fa0-0f39a64b6571',
+        props.user.username,
+        props.user.secret 
+    );
+    return (
+        <div style={{height: '100vh'}}>
+            <MultiChatSocket {...chatProps} />
+            <MultiChatWindow {...chatProps} style={{height: '100%'}} />
+        </div>
+    )
 }
 
 export default ChatsPage
